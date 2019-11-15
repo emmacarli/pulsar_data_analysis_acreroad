52c82407cf72ca3bced66c1a1568d8e5e230b9d8
Emma Carli <emmacarli@users.noreply.github.com>
2019-11-15 16:24:55 +0000

    Added start time of observation

--- Compare_GrahamPreproc_Raw.py
+++ Compare_GrahamPreproc_Raw.py
@@ -5,7 +5,7 @@
 # Author: Emma Carli emma.carli@outlook.com
 #
 # =============================================================================
-#
+
 
 #%% Clear variables
 
@@ -16,6 +16,7 @@ get_ipython().magic('reset -f')
 
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy.time import Time
 
 #%% Set matplotlib general parameters
 
@@ -37,18 +38,23 @@ sampling_rate = 1/sampling_period
 
 #%% Upload the data files
 
-f_raw = open('1183079651.114455-PSRB0329-2ms-sampling-dd.dat', mode='rb')
-f = open('1183079651.114455-preproc.dat', mode='rb')
-data = np.fromfile(f,'f4')
+#Here decide which files you want to compare
+start_time_GPS = 1183079651.114455
+raw_file = str(start_time_GPS)+'-PSRB0329-2ms-sampling-dd.dat'
+preproc_file = str(start_time_GPS)+'-preproc.dat'
+
+f_raw = open(raw_file, mode='rb')
+f_preproc = open(preproc_file, mode='rb')
+data_preproc = np.fromfile(f_preproc,'f4')
 data_raw = np.fromfile(f_raw, 'f4')
 
 #%% Calculate values for axes and ticks
 
 #Preprocessed dataset
-total_seconds = len(data)*sampling_period #total seconds in dataset
-total_hours =  total_seconds / 3600 #total hours in dataset
-total_hours_rounded = np.ceil(total_hours) #total rounded hours in dataset
-total_hours_rounded_datapoints = total_hours_rounded*3600/sampling_period #how many datapoints in total rounded hours in dataset
+total_seconds_preproc = len(data_preproc)*sampling_period #total seconds in dataset
+total_hours_preproc =  total_seconds_preproc / 3600 #total hours in dataset
+total_hours_rounded_preproc = np.ceil(total_hours_preproc) #total rounded hours in dataset
+total_hours_rounded_datapoints_preproc = total_hours_rounded_preproc*3600/sampling_period #how many datapoints in total rounded hours in dataset
 
 #Raw dataset
 total_seconds_raw = len(data_raw)*sampling_period
@@ -60,8 +66,8 @@ total_hours_rounded_datapoints_raw =total_hours_raw_rounded*3600/sampling_period
 #%% Set axes and ticks
 
 #Preprocessed dataset
-major_ticks = np.linspace(0, total_hours_rounded_datapoints, num=total_hours_rounded+1) #set big ticks every hour
-minor_ticks = np.linspace(0, total_hours_rounded_datapoints, num=((total_hours_rounded*60)+1)) #set small ticks every minute
+major_ticks_preproc = np.linspace(0, total_hours_rounded_datapoints_preproc, num=total_hours_rounded_preproc+1) #set big ticks every hour
+minor_ticks_preproc = np.linspace(0, total_hours_rounded_datapoints_preproc, num=((total_hours_rounded_preproc*60)+1)) #set small ticks every minute
 
 #Raw dataset
 major_ticks_raw = np.linspace(0, total_hours_rounded_datapoints_raw, num=total_hours_raw_rounded+1) #set big ticks every hour
@@ -73,20 +79,20 @@ fig1 = plt.figure()
 
 #Preprocessed dataset
 fig1_top = fig1.add_subplot(2,1,1)
-fig1_top.plot(data, color='black',  marker='None', linewidth=0.05) #comment out to get an interactive plot.
-#fig1_top.plot(data, color='black',  linestyle='none', marker=',') #uncomment to get an interactive plot. can't plot individual points in a pdf, it makes it too large to handle. make sure to comment out savefig and close, too.
+#fig1_top.plot(data_preproc, color='black',  marker='None', linewidth=0.05) #uncomment  to get a PDF plot.
+fig1_top.plot(data_preproc, color='black',  linestyle='none', marker=',') #uncomment to get an interactive plot. can't plot individual points in a pdf, it makes it too large to handle.  make sure to comment out savefig and close, too.
 fig1_top.set_ylabel('Power (arbitrary units)')
-fig1_top.set_xticks(major_ticks)
-fig1_top.set_xticks(minor_ticks, minor=True)
+fig1_top.set_xticks(major_ticks_preproc)
+fig1_top.set_xticks(minor_ticks_preproc, minor=True)
 fig1_top.grid(which='minor', alpha=0.2)
 fig1_top.grid(which='major', alpha=0.5)
-fig1_top.set_xticklabels(np.linspace(0, total_hours_rounded, num=total_hours_rounded+1))
+fig1_top.set_xticklabels(np.linspace(0, total_hours_rounded_preproc, num=total_hours_rounded_preproc+1))
 fig1_top.set_title('Preprocessed Data')
 
 #Raw dataset
 fig1_bottom = fig1.add_subplot(2,1,2)
-fig1_bottom.plot(data_raw, color='black', marker='None', linewidth=0.05) #comment out to get an interactive plot.
-#fig1_bottom.plot(data_raw, color='black',  linestyle='none', marker=',') #uncomment to get an interactive plot. can't plot individual points in a pdf, it makes it too large to handle.  make sure to comment out savefig and close, too.
+#fig1_bottom.plot(data_raw, color='black', marker='None', linewidth=0.05) #uncomment  to get a PDF plot.
+fig1_bottom.plot(data_raw, color='black',  linestyle='none', marker=',') #uncomment to get an interactive plot. can't plot individual points in a pdf, it makes it too large to handle.  make sure to comment out savefig and close, too.
 fig1_bottom.set_xlabel('Time (hours)')
 fig1_bottom.set_ylabel('Power (arbitrary units)')
 fig1_bottom.set_xticks(major_ticks_raw)
@@ -96,5 +102,20 @@ fig1_bottom.grid(which='major', alpha=0.5)
 fig1_bottom.set_xticklabels(np.linspace(0, total_hours_raw_rounded, num=total_hours_raw_rounded+1))
 fig1_bottom.set_title('Raw data')
 
-plt.savefig('Compare_GrahamPreproc_Raw.pdf') #comment out if want an interactive plot
-plt.close() #comment out if want an interactive plot
+
+# =============================================================================
+# 
+# HERE THE TIME FORMAT HAS BEEN SET BUT NOT THE SCALE/TIME STANDARD. THIS IS NOT PRECISE -- BUT ONLY FOR A PLOT SO THAT'S OK.
+# 
+# =============================================================================
+
+#now convert the GPS time string  into astropy time format
+start_time_GPS_astropy = Time(start_time_GPS, format='gps') 
+start_time_ISO_astropy = Time(start_time_GPS_astropy, format='iso')
+
+
+fig1.suptitle('Start of recording: ' + start_time_ISO_astropy.value)
+plt.subplots_adjust(hspace=0.5)
+
+#plt.savefig('Compare_GrahamPreproc_Raw.pdf') #comment out if want an interactive plot
+#plt.close() #comment out if want an interactive plot
