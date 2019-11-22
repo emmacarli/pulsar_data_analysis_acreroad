7e79e035e5f23fe8a4e5609214b203652ca1bd56
Emma Carli <emmacarli@users.noreply.github.com>
2019-11-22 15:03:35 +0000

    Copied all raw files locally and plotted all comparisons

--- Compare_GrahamPreproc_Raw/Compare_GrahamPreproc_Raw.py
+++ Compare_GrahamPreproc_Raw/Compare_GrahamPreproc_Raw.py
@@ -15,7 +15,9 @@ get_ipython().magic('reset -f')
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pysftp
 from astropy.time import Time
+import os
 
 #%% Set matplotlib general parameters
 
@@ -30,91 +32,162 @@ plt.rcParams['mathtext.fontset'] = 'stix'
 plt.rcParams['axes.grid'] = True
 plt.rcParams["figure.figsize"] = [10,10]
 
+#%% Which file do you want to plot?
+
+start_time_GPS = 1183079651.114455 #change date here
+file_raw = str(start_time_GPS)+'-PSRB0329-2ms-sampling-dd.dat'
+file_preproc = str(start_time_GPS)+'-preproc.dat'
+
+#%% Connect to ettus
+
+sftp = pysftp.Connection(host='ettus.astro.gla.ac.uk', username='astro') #connect to ettus, no password as I am on the .authorised_keys list
+print('Connection successfully established.')
+sftp.cwd('/home/astro/pulsartelescope/data') #change working directory
+
 #%% Set known data rates
 
 sampling_period = 2e-3 #2 ms
 sampling_rate = 1/sampling_period
 
-#%% Upload the data files
+#%% Get the raw and preprocessed data
 
-#Here decide which files you want to compare
-start_time_GPS = 1183079651.114455
-raw_file = str(start_time_GPS)+'-PSRB0329-2ms-sampling-dd.dat'
-preproc_file = str(start_time_GPS)+'-preproc.dat'
+#Raw data
+handle_file_raw = open('/home/emma/Desktop/Raw_Datafiles/'+file_raw, mode='rb') #change path here to your local copy of raw files
+data_raw = np.fromfile(handle_file_raw,'f4')
 
-f_raw = open(raw_file, mode='rb')
-f_preproc = open(preproc_file, mode='rb')
-data_preproc = np.fromfile(f_preproc,'f4')
-data_raw = np.fromfile(f_raw, 'f4')
+#Preprocessed data - download from ettus temporarily, I don't keep it on my computer for less disk space usage
+file_preproc = str(start_time_GPS)+'-preproc.dat'
+try: 
+    print('Downloading '+file_preproc+'...')
+    sftp.get(file_preproc) #downloads the preprocessed file in the current working directory
+    print(file_preproc+' downloaded.')
+    f_preproc = open(file_preproc, mode='rb')
+    data_preproc = np.fromfile(f_preproc, 'f4')
+    preproc_file_exists = True
+except FileNotFoundError: #if the preprocessed file is not on ettus
+    preproc_file_exists = False  
+    print('Preprocessed file does not exist.')
 
 #%% Calculate values for axes and ticks
 
-#Preprocessed dataset
-total_seconds_preproc = len(data_preproc)*sampling_period #total seconds in dataset
-total_hours_preproc =  total_seconds_preproc / 3600 #total hours in dataset
-total_hours_rounded_preproc = np.ceil(total_hours_preproc) #total rounded hours in dataset
-total_hours_rounded_datapoints_preproc = total_hours_rounded_preproc*3600/sampling_period #how many datapoints in total rounded hours in dataset
+flag = '' #create a flag for data with different lengths
 
 #Raw dataset
 total_seconds_raw = len(data_raw)*sampling_period
 total_hours_raw =  total_seconds_raw / 3600
-total_hours_raw_rounded = np.ceil(total_hours_raw)
-total_hours_rounded_datapoints_raw =total_hours_raw_rounded*3600/sampling_period
+total_hours_rounded_raw = np.ceil(total_hours_raw)
+total_hours_rounded_datapoints_raw =total_hours_rounded_raw*3600/sampling_period
 0
 
+    #Preprocessed dataset
+if preproc_file_exists == True:
+    total_seconds_preproc = len(data_preproc)*sampling_period #total seconds in dataset
+    total_hours_preproc =  total_seconds_preproc / 3600 #total hours in dataset
+    total_hours_rounded_preproc = np.ceil(total_hours_preproc) #total rounded hours in dataset
+    total_hours_rounded_datapoints_preproc = total_hours_rounded_preproc*3600/sampling_period #how many datapoints in total rounded hours in dataset
+    
+    if total_hours_preproc == 8.0:
+        flag = '_preproc_doubled'
+        print('Preprocessed data points doubled. Dividing axes by two...')
+        total_seconds_preproc = total_seconds_preproc/2
+        total_hours_preproc = total_hours_preproc/2
+        total_hours_rounded_preproc =  total_hours_rounded_preproc/2
+        #leave datapoints measure as is
+    if total_hours_rounded_preproc not in [4.0, 5.0, 8.0]:
+        flag = '_different_length'
+        
+if total_hours_rounded_raw not in [4.0, 5.0]:
+    flag = '_different_length'
+        
+    
 #%% Set axes and ticks
 
 #Preprocessed dataset
-major_ticks_preproc = np.linspace(0, total_hours_rounded_datapoints_preproc, num=total_hours_rounded_preproc+1) #set big ticks every hour
-minor_ticks_preproc = np.linspace(0, total_hours_rounded_datapoints_preproc, num=((total_hours_rounded_preproc*60)+1)) #set small ticks every minute
+if preproc_file_exists == True:
+    major_ticks_preproc = np.linspace(0, total_hours_rounded_datapoints_preproc, num=total_hours_rounded_preproc+1) #set big ticks every hour
+    minor_ticks_preproc = np.linspace(0, total_hours_rounded_datapoints_preproc, num=((total_hours_rounded_preproc*60)+1)) #set small ticks every minute
 
 #Raw dataset
-major_ticks_raw = np.linspace(0, total_hours_rounded_datapoints_raw, num=total_hours_raw_rounded+1) #set big ticks every hour
-minor_ticks_raw = np.linspace(0, total_hours_rounded_datapoints_raw, num=((total_hours_raw_rounded*60)+1)) #set small ticks every minute
+major_ticks_raw = np.linspace(0, total_hours_rounded_datapoints_raw, num=total_hours_rounded_raw+1) #set big ticks every hour
+minor_ticks_raw = np.linspace(0, total_hours_rounded_datapoints_raw, num=((total_hours_rounded_raw*60)+1)) #set small ticks every minute
 
 #%% Plot raw and preprocessed files
 
 fig1 = plt.figure()
 
-#Preprocessed dataset
-fig1_top = fig1.add_subplot(2,1,1)
-#fig1_top.plot(data_preproc, color='black',  marker='None', linewidth=0.05) #uncomment  to get a PDF plot.
-fig1_top.plot(data_preproc, color='black',  linestyle='none', marker=',') #uncomment to get an interactive plot. can't plot individual points in a pdf, it makes it too large to handle.  make sure to comment out savefig and close, too.
-fig1_top.set_ylabel('Power (arbitrary units)')
-fig1_top.set_xticks(major_ticks_preproc)
-fig1_top.set_xticks(minor_ticks_preproc, minor=True)
-fig1_top.grid(which='minor', alpha=0.2)
-fig1_top.grid(which='major', alpha=0.5)
-fig1_top.set_xticklabels(np.linspace(0, total_hours_rounded_preproc, num=total_hours_rounded_preproc+1))
-fig1_top.set_title('Preprocessed Data')
+if preproc_file_exists == True: #if there is a preprocessed file corresponding to the raw one, make a plot comparing the two.
+    #Preprocessed dataset
+    fig1_top = fig1.add_subplot(2,1,1)
+    fig1_top.plot(data_preproc, color='black',  marker='None', linewidth=0.05) #uncomment  to get a PDF plot.
+    fig1_top.plot(data_preproc, color='black',  linestyle='none', marker=',') #uncomment to get an interactive plot. can't plot individual points in a pdf, it makes it too large to handle.  make sure to comment out savefig and close, too. 
+    fig1_top.set_ylabel('Power (arbitrary units)')
+    fig1_top.set_xticks(major_ticks_preproc)
+    fig1_top.set_xticks(minor_ticks_preproc, minor=True)
+    fig1_top.grid(which='minor', alpha=0.2)
+    fig1_top.grid(which='major', alpha=0.5)
+    fig1_top.set_xticklabels(np.linspace(0, total_hours_rounded_preproc, num=total_hours_rounded_preproc+1))
+    fig1_top.set_title('Preprocessed Data')
+    
+    #Raw dataset
+    fig1_bottom = fig1.add_subplot(2,1,2)
+    #fig1_bottom.plot(data_raw, color='black', marker='None', linewidth=0.05) #uncomment  to get a PDF plot.
+    fig1_bottom.plot(data_raw, color='black',  linestyle='none', marker=',') #uncomment to get an interactive plot. can't plot individual points in a pdf, it makes it too large to handle.  make sure to comment out savefig and close, too.
+    fig1_bottom.set_xlabel('Time since start (hours)')
+    fig1_bottom.set_ylabel('Power (arbitrary units)')
+    fig1_bottom.set_xticks(major_ticks_raw)
+    fig1_bottom.set_xticks(minor_ticks_raw, minor=True)
+    fig1_bottom.grid(which='minor', alpha=0.2)
+    fig1_bottom.grid(which='major', alpha=0.5)
+    fig1_bottom.set_xticklabels(np.linspace(0, total_hours_rounded_raw, num=total_hours_rounded_raw+1))
+    fig1_bottom.set_title('Raw data')
+    
+    # =============================================================================
+    # 
+    # HERE THE TIME FORMAT HAS BEEN SET BUT NOT THE SCALE/TIME STANDARD. THIS IS NOT PRECISE -- BUT ONLY FOR A PLOT SO THAT'S OK.
+    # 
+    # =============================================================================
+    
+    #now convert the GPS time string  into astropy time format
+    start_time_GPS_astropy = Time(start_time_GPS, format='gps') 
+    start_time_ISO_astropy = Time(start_time_GPS_astropy, format='iso')
+    
+    
+    fig1.suptitle('Start of recording: ' + start_time_ISO_astropy.value)
+    plt.subplots_adjust(hspace=0.5)
+    
+    
+    #plt.savefig('Plots/'+str(start_time_GPS)+'_Compare_GrahamPreproc_Raw'+flag+'.pdf') #comment out if want interactive plot
+    
+else: #if there is no preprocessed file,  plot the raw one on its own.
+    ax1 = plt.gca()
+    #ax1.plot(data_raw, color='black',  marker='None', linewidth=0.05) #uncomment  to get a PDF plot.
+    ax1.plot(data_raw, color='black',  linestyle='none', marker=',') #uncomment to get an interactive plot. can't plot individual points in a pdf, it makes it too large to handle.  make sure to comment out savefig and close, too.
+    ax1.set_ylabel('Power (arbitrary units)')
+    ax1.set_xticks(major_ticks_raw)
+    ax1.set_xticks(minor_ticks_raw, minor=True)
+    ax1.set_xlabel('Time (hours)')
+    ax1.grid(which='minor', alpha=0.2)
+    ax1.grid(which='major', alpha=0.5)
+    ax1.set_xticklabels(np.linspace(0, total_hours_rounded_raw, num=total_hours_rounded_raw+1))
+    
+    # =============================================================================
+    # 
+    # HERE THE TIME FORMAT HAS BEEN SET BUT NOT THE SCALE/TIME STANDARD. THIS IS NOT PRECISE -- BUT ONLY FOR A PLOT SO THAT'S OK.
+    # 
+    # =============================================================================
+    
+    #now convert the GPS time string  into astropy time format
+    start_time_GPS_astropy = Time(start_time_GPS, format='gps') 
+    start_time_ISO_astropy = Time(start_time_GPS_astropy, format='iso')
+    
+    
+    ax1.set_title('Preprocessed Data, Start of recording: ' + start_time_ISO_astropy.value)
+    
+    #plt.savefig('Plots/'+str(start_time_GPS)+'_GrahamRaw'+flag+'.pdf') #comment out if want interactive plot
+    
 
-#Raw dataset
-fig1_bottom = fig1.add_subplot(2,1,2)
-#fig1_bottom.plot(data_raw, color='black', marker='None', linewidth=0.05) #uncomment  to get a PDF plot.
-fig1_bottom.plot(data_raw, color='black',  linestyle='none', marker=',') #uncomment to get an interactive plot. can't plot individual points in a pdf, it makes it too large to handle.  make sure to comment out savefig and close, too.
-fig1_bottom.set_xlabel('Time (hours)')
-fig1_bottom.set_ylabel('Power (arbitrary units)')
-fig1_bottom.set_xticks(major_ticks_raw)
-fig1_bottom.set_xticks(minor_ticks_raw, minor=True)
-fig1_bottom.grid(which='minor', alpha=0.2)
-fig1_bottom.grid(which='major', alpha=0.5)
-fig1_bottom.set_xticklabels(np.linspace(0, total_hours_raw_rounded, num=total_hours_raw_rounded+1))
-fig1_bottom.set_title('Raw data')
-
-
-# =============================================================================
-# 
-# HERE THE TIME FORMAT HAS BEEN SET BUT NOT THE SCALE/TIME STANDARD. THIS IS NOT PRECISE -- BUT ONLY FOR A PLOT SO THAT'S OK.
-# 
-# =============================================================================
-
-#now convert the GPS time string  into astropy time format
-start_time_GPS_astropy = Time(start_time_GPS, format='gps') 
-start_time_ISO_astropy = Time(start_time_GPS_astropy, format='iso')
-
-
-fig1.suptitle('Start of recording: ' + start_time_ISO_astropy.value)
-plt.subplots_adjust(hspace=0.5)
-
-#plt.savefig('Compare_GrahamPreproc_Raw.pdf') #comment out if want an interactive plot
+os.remove(file_preproc) #sftp creates an empty file when trying download even if preproc file does not exist on ettus
 #plt.close() #comment out if want an interactive plot
+print('File(s) plotted and deleted.')
+
+sftp.close()
--- Compare_GrahamPreproc_Raw_allfiles/Compare_GrahamPreproc_Raw_allfiles_sftp.py
+++ Compare_GrahamPreproc_Raw_allfiles/Compare_GrahamPreproc_Raw_allfiles_sftp.py
@@ -2,7 +2,7 @@
 # 
 # This code plots raw data and data preprocessed using Graham's routine.
 # PDFs are useful to save all the plots but interactive plots can be looked at with more precision, although individually.
-# This code does not need to be run from ettus. It must be run on campus or using a VPN.
+# This code does not need to be run from ettus. I have a local copy of the raw files and download Graham's preprocessed files on campus or using a VPN. 
 # Author: Emma Carli emma.carli@outlook.com
 #
 # =============================================================================
@@ -20,6 +20,7 @@ import matplotlib.pyplot as plt
 import pysftp
 from astropy.time import Time
 import os
+import glob
 
 #%% Set matplotlib general parameters
 
@@ -55,72 +56,87 @@ for file in sftp.listdir():
 sampling_period = 2e-3 #2 ms
 sampling_rate = 1/sampling_period
 
-for file_preproc in preproc_files:
-    
-    start_time_GPS = float(file_preproc[:-12])
+#%% Get the raw and preprocessed data
 
-    #if this file has already been plotted, skip it and go to the next in preproc_files
-    if os.path.exists(str(start_time_GPS)+'_GrahamPreproc.pdf') or os.path.exists(str(start_time_GPS)+'_Compare_GrahamPreproc_Raw.pdf'):
+for file_raw in raw_files:
+    
+    start_time_GPS = float(file_raw[:-29])
+
+    #if this file has already been plotted, skip it and go to the next in raw_files
+    if glob.glob('Plots/'+str(start_time_GPS)+'*.pdf') != []:
         print(str(start_time_GPS)+' already plotted.')
         continue
     
-    #%% Upload the data file
-    print('Downloading '+file_preproc+'...')
-    sftp.get(file_preproc) #downloads the preprocessed file in the current working directory
-    print(file_preproc+' downloaded.')
-    
-    
-    #Preprocessed data
-    handle_file_preproc = open(file_preproc, mode='rb')
-    data_preproc = np.fromfile(handle_file_preproc,'f4')
+
     
     #Raw data
-    file_raw = str(start_time_GPS)+'-PSRB0329-2ms-sampling-dd.dat'
-    try: 
-        print('Downloading '+file_raw+'...')
-        sftp.get(file_raw) #downloads the raw file in the current working directory
-        print(file_raw+' downloaded.')
-        f_raw = open(file_raw, mode='rb')
-        raw_file_exists = True
-    except FileNotFoundError:
-        raw_file_exists = False  
-        print('Raw file does not exist.')
+    handle_file_raw = open('/home/emma/Desktop/Raw_Datafiles/'+file_raw, mode='rb') #change path here to your local copy of raw files
+    data_raw = np.fromfile(handle_file_raw,'f4')
     
-    if raw_file_exists == True:
-        data_raw = np.fromfile(f_raw, 'f4')
+    #Preprocessed data - download from ettus temporarily, I don't keep it on my computer for less disk space usage
+    file_preproc = str(start_time_GPS)+'-preproc.dat'
+    try: 
+        print('Downloading '+file_preproc+'...')
+        sftp.get(file_preproc) #downloads the preprocessed file in the current working directory
+        print(file_preproc+' downloaded.')
+        f_preproc = open(file_preproc, mode='rb')
+        data_preproc = np.fromfile(f_preproc, 'f4')
+        preproc_file_exists = True
+    except FileNotFoundError: #if the preprocessed file is not on ettus
+        preproc_file_exists = False  
+        print('Preprocessed file does not exist.')
     
     #%% Calculate values for axes and ticks
     
-    #Preprocessed dataset
-    total_seconds_preproc = len(data_preproc)*sampling_period #total seconds in dataset
-    total_hours_preproc =  total_seconds_preproc / 3600 #total hours in dataset
-    total_hours_rounded_preproc = np.ceil(total_hours_preproc) #total rounded hours in dataset
-    total_hours_rounded_datapoints_preproc = total_hours_rounded_preproc*3600/sampling_period #how many datapoints in total rounded hours in dataset
+    flag = '' #create a flag for data with different lengths
     
     #Raw dataset
-    if raw_file_exists == True:
-        total_seconds_raw = len(data_raw)*sampling_period
-        total_hours_raw =  total_seconds_raw / 3600
-        total_hours_raw_rounded = np.ceil(total_hours_raw)
-        total_hours_rounded_datapoints_raw =total_hours_raw_rounded*3600/sampling_period
-        0
+    total_seconds_raw = len(data_raw)*sampling_period
+    total_hours_raw =  total_seconds_raw / 3600
+    total_hours_rounded_raw = np.ceil(total_hours_raw)
+    total_hours_rounded_datapoints_raw =total_hours_rounded_raw*3600/sampling_period
+    0
     
+        #Preprocessed dataset
+    if preproc_file_exists == True:
+        total_seconds_preproc = len(data_preproc)*sampling_period #total seconds in dataset
+        total_hours_preproc =  total_seconds_preproc / 3600 #total hours in dataset
+        total_hours_rounded_preproc = np.ceil(total_hours_preproc) #total rounded hours in dataset
+        total_hours_rounded_datapoints_preproc = total_hours_rounded_preproc*3600/sampling_period #how many datapoints in total rounded hours in dataset
+        
+        if total_hours_preproc == 8.0:
+            flag = '_preproc_doubled'
+            print('Preprocessed data points doubled. Dividing axes by two...')
+            total_seconds_preproc = total_seconds_preproc/2
+            total_hours_preproc = total_hours_preproc/2
+            total_hours_rounded_preproc =  total_hours_rounded_preproc/2
+            #leave datapoints measure as is
+        if total_hours_rounded_preproc not in [4.0, 5.0, 8.0]:
+            flag = '_different_length'
+            
+    if total_hours_rounded_raw not in [4.0, 5.0]:
+        flag = '_different_length'
+            
+    
+    
+            
+        
     #%% Set axes and ticks
     
     #Preprocessed dataset
-    major_ticks_preproc = np.linspace(0, total_hours_rounded_datapoints_preproc, num=total_hours_rounded_preproc+1) #set big ticks every hour
-    minor_ticks_preproc = np.linspace(0, total_hours_rounded_datapoints_preproc, num=((total_hours_rounded_preproc*60)+1)) #set small ticks every minute
+    if preproc_file_exists == True:
+        major_ticks_preproc = np.linspace(0, total_hours_rounded_datapoints_preproc, num=total_hours_rounded_preproc+1) #set big ticks every hour
+        minor_ticks_preproc = np.linspace(0, total_hours_rounded_datapoints_preproc, num=((total_hours_rounded_preproc*60)+1)) #set small ticks every minute
     
     #Raw dataset
-    if raw_file_exists == True:
-        major_ticks_raw = np.linspace(0, total_hours_rounded_datapoints_raw, num=total_hours_raw_rounded+1) #set big ticks every hour
-        minor_ticks_raw = np.linspace(0, total_hours_rounded_datapoints_raw, num=((total_hours_raw_rounded*60)+1)) #set small ticks every minute
+    major_ticks_raw = np.linspace(0, total_hours_rounded_datapoints_raw, num=total_hours_rounded_raw+1) #set big ticks every hour
+    minor_ticks_raw = np.linspace(0, total_hours_rounded_datapoints_raw, num=((total_hours_rounded_raw*60)+1)) #set small ticks every minute
     
     #%% Plot raw and preprocessed files
     
     fig1 = plt.figure()
     
-    if raw_file_exists == True: #if there is a raw file corresponding to the preprocessed one, make a plot comparing the two.
+    if preproc_file_exists == True: #if there is a preprocessed file corresponding to the raw one, make a plot comparing the two.
         #Preprocessed dataset
         fig1_top = fig1.add_subplot(2,1,1)
         fig1_top.plot(data_preproc, color='black',  marker='None', linewidth=0.05) 
@@ -135,13 +151,13 @@ for file_preproc in preproc_files:
         #Raw dataset
         fig1_bottom = fig1.add_subplot(2,1,2)
         fig1_bottom.plot(data_raw, color='black', marker='None', linewidth=0.05) 
-        fig1_bottom.set_xlabel('Time (hours)')
+        fig1_bottom.set_xlabel('Time since start (hours)')
         fig1_bottom.set_ylabel('Power (arbitrary units)')
         fig1_bottom.set_xticks(major_ticks_raw)
         fig1_bottom.set_xticks(minor_ticks_raw, minor=True)
         fig1_bottom.grid(which='minor', alpha=0.2)
         fig1_bottom.grid(which='major', alpha=0.5)
-        fig1_bottom.set_xticklabels(np.linspace(0, total_hours_raw_rounded, num=total_hours_raw_rounded+1))
+        fig1_bottom.set_xticklabels(np.linspace(0, total_hours_rounded_raw, num=total_hours_rounded_raw+1))
         fig1_bottom.set_title('Raw data')
         
         # =============================================================================
@@ -159,18 +175,18 @@ for file_preproc in preproc_files:
         plt.subplots_adjust(hspace=0.5)
         
         
-        plt.savefig('plots/'+str(start_time_GPS)+'_Compare_GrahamPreproc_Raw.pdf') 
+        plt.savefig('Plots/'+str(start_time_GPS)+'_Compare_GrahamPreproc_Raw'+flag+'.pdf') 
         
-    else: #if there is no raw file,  plot the preprocessed one on its own.
+    else: #if there is no preprocessed file,  plot the raw one on its own.
         ax1 = plt.gca()
-        ax1.plot(data_preproc, color='black',  marker='None', linewidth=0.05) 
+        ax1.plot(data_raw, color='black',  marker='None', linewidth=0.05) 
         ax1.set_ylabel('Power (arbitrary units)')
-        ax1.set_xticks(major_ticks_preproc)
-        ax1.set_xticks(minor_ticks_preproc, minor=True)
+        ax1.set_xticks(major_ticks_raw)
+        ax1.set_xticks(minor_ticks_raw, minor=True)
         ax1.set_xlabel('Time (hours)')
         ax1.grid(which='minor', alpha=0.2)
         ax1.grid(which='major', alpha=0.5)
-        ax1.set_xticklabels(np.linspace(0, total_hours_rounded_preproc, num=total_hours_rounded_preproc+1))
+        ax1.set_xticklabels(np.linspace(0, total_hours_rounded_raw, num=total_hours_rounded_raw+1))
         
         # =============================================================================
         # 
@@ -185,12 +201,11 @@ for file_preproc in preproc_files:
         
         ax1.set_title('Preprocessed Data, Start of recording: ' + start_time_ISO_astropy.value)
         
-        plt.savefig('plots/'+str(start_time_GPS)+'_GrahamPreproc.pdf') 
+        plt.savefig('Plots/'+str(start_time_GPS)+'_GrahamRaw'+flag+'.pdf') 
         
 
-    os.remove(file_preproc)
-    os.remove(file_raw) #sftp creates an empty file when trying download even if raw file does not exist on ettus
-    plt.close() #comment out if want an interactive plot
+    os.remove(file_preproc) #sftp creates an empty file when trying download even if preproc file does not exist on ettus
+    plt.close() #comment out if want an interactive plot in debugging
     print('File(s) plotted and deleted.')
 
 sftp.close()
