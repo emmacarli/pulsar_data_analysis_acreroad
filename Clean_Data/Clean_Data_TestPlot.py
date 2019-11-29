cf42404be8eb8bc18bbf3c79631a2232a2398794
Emma Carli <emmacarli@users.noreply.github.com>
2019-11-29 18:35:00 +0000

    More plotting possibilities

--- Clean_Data/Clean_Data_TestPlot.py
+++ Clean_Data/Clean_Data_TestPlot.py
@@ -1,3 +1,6 @@
+ticks_in_minutes = True #decide whether to label data in minutes or hours
+
+
 # =============================================================================
 # 
 # This code cleans one raw pulsar data file and plots what happens to the data as it's cleaned.
@@ -43,21 +46,23 @@ ax1 = plt.gca()
     
 #%% Which raw file do you want to clean and plot?
 
-start_time_GPS = 1185177007.112782 #change date here
+start_time_GPS = 1195086376.112684 #change date here
 file_raw = str(start_time_GPS)+'-PSRB0329-2ms-sampling-dd.dat'
 handle_file_raw = open('/home/emma/Desktop/Raw_Datafiles/'+file_raw, mode='rb') 
 data_raw = np.fromfile(handle_file_raw,'f4')
 ax1.plot(data_raw, label='Raw data')
+#ax1.plot(data_raw, label='Raw data', linestyle = 'none',   marker='s', markersize=72/fig1.dpi, mec='None') #comment out depending on what you want on your plot
+#using a square without border at the pixel size for a marker is a workaround using Matplotlib's pixel marker ',' which size cannot be increased in the legend.
 
 #%% Crop the raw file to a length that is an integer number of minutes
 #so that later can sigma clip over one minute interval
 number_of_one_minute_intervals = int( np.floor( len(data_raw) / one_minute_in_datapoints ))
 data1 = data_raw[0: number_of_one_minute_intervals * one_minute_in_datapoints]
-ax1.plot(data1, label='Cropped to integer number of minutes')
+ax1.plot(data1, label='Cropped to integer number of minutes') #comment out depending on what you want on your plot
 
 #%% Remove median of whole dataset
 data2 = data1 - np.median(data1)
-ax1.plot(data2, label='Median subtracted')
+ax1.plot(data2, label='Median subtracted') #comment out depending on what you want on your plot
 
 #%% Median filter over a 1 second interval
 
@@ -71,7 +76,7 @@ for second_long_chunk_start in second_long_chunks_starting_points :
     median_filter_1s[second_long_chunk_start : second_long_chunk_end] = np.median(data2[second_long_chunk_start : second_long_chunk_end]) 
 
 data3 = data2 - median_filter_1s
-ax1.plot(data3, label='One second median filtered')
+ax1.plot(data3, label='One second median filtered') #comment out depending on what you want on your plot
 
 # =============================================================================
 #     #this was very long so made my own:
@@ -89,6 +94,7 @@ for minute_long_chunk_start in minute_long_chunks_starting_points :
     sigma_masked_array.set_fill_value(0.0)
     data4[minute_long_chunk_start : minute_long_chunk_end] = sigma_masked_array.filled()
 ax1.plot(data4, label='One minute 5-sigma clip filtered')
+#ax1.plot(data4, label='One minute 5-sigma clip filtered', linestyle = 'none',   marker='s', markersize=72/fig1.dpi, mec='None') #comment out depending on what you want on your plot
 
 #%% Add details to the finished plot
 
@@ -102,7 +108,8 @@ ax1.plot(data4, label='One minute 5-sigma clip filtered')
 start_time_GPS_astropy = Time(start_time_GPS, format='gps') 
 start_time_ISO_astropy = Time(start_time_GPS_astropy, format='iso')
 
-plt.legend(loc='upper right')
+
+ax1.legend(markerscale=12,loc='upper right')
 ax1.set_ylabel('Power (arbitrary units)')
 ax1.set_title('Data cleaning steps on raw file starting '+start_time_ISO_astropy.value)
 
@@ -116,10 +123,17 @@ total_hours_rounded_datapoints =total_hours_rounded*3600/sampling_period
 major_ticks = np.linspace(0, total_hours_rounded_datapoints, num=total_hours_rounded+1) #set big ticks every hour
 minor_ticks = np.linspace(0, total_hours_rounded_datapoints, num=((total_hours_rounded*60)+1)) #set small ticks every minute
 
-ax1.set_xticks(major_ticks)
-ax1.set_xticks(minor_ticks, minor=True)
-ax1.grid(which='minor', alpha=0.2)
-ax1.grid(which='major', alpha=0.5)
-ax1.set_xticklabels(np.linspace(0, total_hours_rounded, num=total_hours_rounded+1))
-ax1.set_xlabel('Time since start (hours)')
+
+if ticks_in_minutes == False:
+    ax1.set_xticks(major_ticks)
+    ax1.set_xticks(minor_ticks, minor=True)
+    ax1.grid(which='minor', alpha=0.2)
+    ax1.grid(which='major', alpha=0.5)
+    ax1.set_xticklabels(np.linspace(0, total_hours_rounded, num=total_hours_rounded+1))
+    ax1.set_xlabel('Time since start (hours)')
+else:
+   ax1.set_xticks(minor_ticks)
+   ax1.grid(which='major', alpha=0.5)
+   ax1.set_xticklabels(np.linspace(0, total_hours_rounded*60, num=(total_hours_rounded*60)+1))
+   ax1.set_xlabel('Time since start (minutes)')
 
