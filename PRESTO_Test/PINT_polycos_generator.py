bfdc8bd0606d55c7ae7b138079b7417d7bb963a9
Emma Carli <emmacarli@users.noreply.github.com>
2020-01-09 16:14:22 +0100

    PINT polycos generated

--- PRESTO_Test/PINT_polycos_generator.py
+++ PRESTO_Test/PINT_polycos_generator.py
@@ -24,28 +24,30 @@ sys.stdout = log_handle
 sys.stderr = log_handle
 
 
-#%% Observatory definition 
-acre_latitude = 55.902528 #from clicking on pulsar telescope at acre road in Google Maps
-acre_longitude = -4.307107 #from clicking on pulsar telescope at acre road in Google Maps
-acre_height = 50 #meters
-acre_EarthLocation = astropy.coordinates.EarthLocation.from_geodetic(lon=acre_longitude, lat=acre_latitude, height=acre_height)
-acre_itrs = acre_EarthLocation.get_itrs() #wonder if itrs and itrf is the same thing...
-
-
-observatory.topo_obs.TopoObs('acre', aliases=['AR', 'acreroad'], itrf_xyz = acre_itrs.data.xyz )
-
-#Normally, an observatory only has to be defined once. 
-#A list of observatories created in the past can be found under pint/observatory/observatories.py
-#It didn't seem to save for me, so I entered it manually.
+# =============================================================================
+# #%% Observatory definition 
+# acre_latitude = 55.902528 #from clicking on pulsar telescope at acre road in Google Maps
+# acre_longitude = -4.307107 #from clicking on pulsar telescope at acre road in Google Maps
+# acre_height = 50 #meters
+# acre_EarthLocation = astropy.coordinates.EarthLocation.from_geodetic(lon=acre_longitude, lat=acre_latitude, height=acre_height)
+# acre_itrs = acre_EarthLocation.get_itrs() #wonder if itrs and itrf is the same thing...
+# 
+# 
+# observatory.topo_obs.TopoObs('acre', aliases=['AR', 'acreroad'], itrf_xyz = acre_itrs.data.xyz )
+# 
+# #Normally, an observatory only has to be defined once. 
+# #A list of observatories created in the past can be found under pint/observatory/observatories.py
+# #It didn't seem to save for me, so I entered it manually.
+# =============================================================================
 
 #%%Generate model
 B032954_model = models.model_builder.get_model('B0329+54.par')
+#this parameter file's data was obtained from ATNF
 
 #%% Generate polycos
 pl = polycos.Polycos() #initiate an empty polycos instance
-polyco_table = pl.generate_polycos(model = B032954_model, mjdStart = 57969.1, mjdEnd = 57969.4, obs = 'acre', segLength = 10, ncoeff = 12, obsFreq = 407.5, method = 'TEMPO') #note segment length in minutes, observing frequency in MHz, method TEMPO2 unavailable
-#the parameter file was obtained from ATNF
+pl.generate_polycos(model = B032954_model, mjdStart = 57969.1, mjdEnd = 57969.4, obs = 'acre', segLength = 10, ncoeff = 5, obsFreq = 407.5, method = 'TEMPO') #note segment length in minutes, observing frequency in MHz, method TEMPO2 unavailable
 
 #%% Write to TEMPO style polyco table (PRESTO uses these for folding)
-#polycos.tempo_polyco_table_writer(polyco_table, filename='PINT_polyco.dat')
+pl.write_polyco_file(format='tempo', filename='PINT_polycos.dat')
 
