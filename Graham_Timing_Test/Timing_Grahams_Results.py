2445d0c2eebf5ed5418120efccae0956f0133ab3
Emma Carli <emmacarli@users.noreply.github.com>
2020-03-24 14:36:00 +0000

    Timing test on Graham's previous results

--- Graham_Timing_Test/Timing_Grahams_Results.py
+++ Graham_Timing_Test/Timing_Grahams_Results.py
@@ -12,7 +12,7 @@ from astropy.visualization import quantity_support
 
 
 # Import model
-model = models.model_builder.get_model('J0332+5434_initial_parameters.par')
+model = models.model_builder.get_model('J0332+5434_Grahams_initial_parameters.par')
 
 # Import TOAs
 TOAs = toa.get_TOAs('J0332+5434_Grahams_Observations.tim', planets=True)
@@ -41,24 +41,22 @@ plt.legend()
 #plt.close()
 
 
-# =============================================================================
-# # Perform the fit
-# WLS_fit = fitter.WLSFitter(TOAs, model)
-# WLS_fit.set_fitparams('F0','F1', 'RAJ', 'DECJ') 
-# WLS_fit.fit_toas()
-# 
-# fig4 = plt.figure()
-# ax4 = plt.gca()
-# 
-# # Plot the post-fit residuals
-# ax4.errorbar(TOAs.get_mjds().value, WLS_fit.resids.time_resids.to(units.us), yerr=TOAs.get_errors().to(units.us), color='black', elinewidth=0.05, capsize=0, marker='o',markersize=0.8, linestyle='none')
-# #ax4.scatter(TOAs.get_mjds().value, WLS_fit.resids.time_resids.to(units.us), marker='.', color='black')
-# ax4.set_title("%s Post-Fit Timing Residuals" % model.PSR.value)
-# ax4.set_xlabel("MJD")
-# ax4.set_ylabel("Residual ($\mu$s)")
-# plt.savefig('Post_fit_residuals.pdf')
-# plt.savefig('Post_fit_residuals_no_error_bars.pdf') 
-# plt.close()
-# 
-# =============================================================================
+# Perform the fit
+WLS_fit = fitter.WLSFitter(TOAs, model)
+WLS_fit.set_fitparams('F0','F1', 'RAJ', 'DECJ') 
+WLS_fit.fit_toas()
+
+fig4 = plt.figure()
+ax4 = plt.gca()
+
+# Plot the post-fit residuals
+ax4.errorbar(TOAs.get_mjds().value, WLS_fit.resids.time_resids.to(units.us), yerr=TOAs.get_errors().to(units.us), color='black', elinewidth=0.05, capsize=0, marker='o',markersize=0.8, linestyle='none')
+#ax4.scatter(TOAs.get_mjds().value, WLS_fit.resids.time_resids.to(units.us), marker='.', color='black')
+ax4.set_title("%s Post-Fit Timing Residuals" % model.PSR.value)
+ax4.set_xlabel("MJD")
+ax4.set_ylabel("Residual ($\mu$s)")
+plt.savefig('Post_fit_residuals.pdf')
+plt.savefig('Post_fit_residuals_no_error_bars.pdf') 
+plt.close()
+
 
